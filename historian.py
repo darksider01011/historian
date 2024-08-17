7512ef9b852f58ce0734eb0a353f51d114644289
mostafa taghizadeh <132552898+darksider01011@users.noreply.github.com>
2024-08-17 14:36:20 -0700

    Update historian.py

--- historian.py
+++ historian.py
@@ -1,11 +1,12 @@
-import argparse
-from wayback import WaybackClient
-from datetime import date, timedelta
-
-client = WaybackClient()
-
-def From(fromm):
-    date = str(fromm)
+import argparse                                                                                                                                                                                                                             
+from wayback import WaybackClient                                                                                                                                                                                                           
+from datetime import date, timedelta  
+from colorama import Fore, Back, Style
+                                                                                                                                                                                                                                            
+client = WaybackClient()                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                            
+def From(fromm):                                                                                                                                                                                                                            
+    date = str(fromm)                                                                                                                                                                                                                       
     datee = len(date)
     if datee > 8 or datee < 8:
         raise argparse.ArgumentTypeError('invalid from date format.   example: 20150510')
@@ -19,7 +20,7 @@ def To(to):
     else:
         return date
 
-parser = argparse.ArgumentParser(description='check history of robots.txt from wayback', prog= 'Gadget.py', epilog= 'Example: python3 Gadget.py  -d nsa.gov -f 20050103 -t 20050203')
+parser = argparse.ArgumentParser(description='historian.py is python script that use wayback library to check robots.txt history', prog= 'historian.py', epilog= 'Example: python3 historian.py  -d amazon.com -f 20050103 -t 20050203')
 parser.add_argument('-d', type=str, help='Set target domain', metavar= 'example.com', default=False, required=True)
 parser.add_argument('-f', type=From, help='From date', metavar= '20020501', default=False, required=True)
 parser.add_argument('-t', type=To, help='To date', metavar= '20240501', default=False, required=True)
@@ -43,4 +44,4 @@ end_date = date.fromisoformat(too)
 for single_date in daterange(start_date, end_date):
     results = client.search(domain + "/robots.txt",  from_date=single_date)
     record = next(results)
-    print(record.timestamp, record.view_url)
+    print(Fore.BLUE + Style.BRIGHT + str(record.timestamp) +" "+ Fore.WHITE + str(record.view_url))
